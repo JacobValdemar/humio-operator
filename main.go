07f67ac19f31d06cae28f4b8311552da41b3724f
Jestin Woods <jestin.woods@crowdstrike.com>
2023-05-18 10:25:34 -0700

    wip

--- main.go
+++ main.go
@@ -27,6 +27,7 @@ import (
 	humioapi "github.com/humio/cli/api"
 	cmapi "github.com/jetstack/cert-manager/pkg/apis/certmanager/v1"
 	openshiftsecurityv1 "github.com/openshift/api/security/v1"
+
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
@@ -190,6 +191,13 @@ func main() {
 		BaseLogger:  log,
 	}).SetupWithManager(mgr); err != nil {
 		ctrl.Log.Error(err, "unable to create controller", "controller", "HumioAlert")
+		os.Exit(1)
+	}
+	if err = (&controllers.HumioBootstrapTokenReconciler{
+		Client:     mgr.GetClient(),
+		BaseLogger: log,
+	}).SetupWithManager(mgr); err != nil {
+		ctrl.Log.Error(err, "unable to create controller", "controller", "HumioBootstrapToken")
 		os.Exit(1)
 	}
 	//+kubebuilder:scaffold:builder
